650f83e3201145630e0287a5118c5bdadb47cf94
Sergey Vladimirov <vladimirov.exceedteam@gmail.com>
2022-05-11 16:18:00 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,26 +6,26 @@ import { Hospital } from "components/pics";
 import "./style.scss";
 
 const Header = ({ headText, logout }) => {
-	const nav = useNavigate();
-	const goOut = () => {
-		axios.post(`${url}/logout`).then((res) => {
-			nav("/");
-		});
-	};
+  const nav = useNavigate();
+  const goOut = () => {
+    axios.post(`${url}/logout`).then((res) => {
+      nav("/");
+    });
+  };
 
-	return (
-		<header className='header'>
-			<Hospital need='logo' />
-			<h2 className='header__headtext'>{headText}</h2>
-			{logout ? (
-				<Button onClick={goOut} variant='contained'>
-					Bыход
-				</Button>
-			) : (
-				false
-			)}
-		</header>
-	);
+  return (
+    <header className='header'>
+      <Hospital need='logo' />
+      <h2 className='header__headtext'>{headText}</h2>
+      {logout ? (
+        <Button onClick={goOut} variant='contained'>
+          Bыход
+        </Button>
+      ) : (
+        false
+      )}
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -6,237 +6,237 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Snack from "components/Snack/Snack";
 import { url } from "components/helper/constants";
 import {
-	userNameValidate,
-	passwValidate,
+  userNameValidate,
+  passwValidate,
 } from "components/helper/validate";
 import "./style.scss";
 
 const Signup = () => {
-	const nav = useNavigate();
-	const [showPass, setShowPass] = useState("password");
-	const [notice, setNotice] = useState("");
-	const [openSnack, setOpenSnack] = useState(false);
-	const [userfield, setUserField] = useState({
-		username: "",
-		password: "",
-		secondPassword: "",
-	});
-	const [isValid, setIsValid] = useState(false);
-	const [errors, setErrors] = useState({
-		username: "",
-		password: "",
-		secondPassword: "",
-	});
+  const nav = useNavigate();
+  const [showPass, setShowPass] = useState("password");
+  const [notice, setNotice] = useState("");
+  const [openSnack, setOpenSnack] = useState(false);
+  const [userfield, setUserField] = useState({
+    username: "",
+    password: "",
+    secondPassword: "",
+  });
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+    secondPassword: "",
+  });
 
-	const registr = (e) => {
-		e.preventDefault();
-		if (isValid) {
-			const { username, password } = userfield;
-			axios
-				.post(`${url}/registration`, {
-					name: username,
-					password: password,
-				})
-				.then((res) => {
-					localStorage.setItem(
-						"accesToken",
-						res.data.token.accessToken
-					);
-					nav("/appointments");
-				})
-				.catch((err) => {
-					setNotice("Такой юзверь уже существует.");
-					setOpenSnack(true);
-				});
-		}
-	};
+  const registr = (e) => {
+    e.preventDefault();
+    if (isValid) {
+      const { username, password } = userfield;
+      axios
+        .post(`${url}/registration`, {
+          name: username,
+          password: password,
+        })
+        .then((res) => {
+          localStorage.setItem(
+            "accesToken",
+            res.data.token.accessToken
+          );
+          nav("/appointments");
+        })
+        .catch((err) => {
+          setNotice("Такой юзверь уже существует.");
+          setOpenSnack(true);
+        });
+    }
+  };
 
-	const showPassfunction = () => {
-		showPass !== "text"
-			? setShowPass("text")
-			: setShowPass("password");
-	};
+  const showPassfunction = () => {
+    showPass !== "text"
+      ? setShowPass("text")
+      : setShowPass("password");
+  };
 
-	const blurHandler = (e) => {
-		const candidate = e.target.name;
-		const { username, password, secondPassword } = userfield;
-		if (candidate === "username") {
-			if (userNameValidate(username)) {
-				setErrors({
-					...errors,
-					username: "",
-				});
-			} else {
-				setErrors({
-					...errors,
-					username:
-						"Поле Login должно быть от 6 символов, разрешены только латинские буквы",
-				});
-				setIsValid(false);
-			}
-		}
-		if (candidate === "password") {
-			if (passwValidate(password)) {
-				setErrors({
-					...errors,
-					password: "",
-				});
-			} else {
-				setErrors({
-					...errors,
-					password:
-						"Пароль должен быть от 6 до 12 символов латинские буквы и цифры",
-				});
-				setIsValid(false);
-			}
-		}
-		if (candidate === "secondPassword") {
-			if (secondPassword === password) {
-				setErrors({
-					...errors,
-					secondPassword: "",
-				});
-			} else {
-				setErrors({
-					...errors,
-					secondPassword: "Введённые пароли должны совпадать",
-				});
-				setIsValid(false);
-			}
-		}
+  const blurHandler = (e) => {
+    const candidate = e.target.name;
+    const { username, password, secondPassword } = userfield;
+    if (candidate === "username") {
+      if (userNameValidate(username)) {
+        setErrors({
+          ...errors,
+          username: "",
+        });
+      } else {
+        setErrors({
+          ...errors,
+          username:
+            "Поле Login должно быть от 6 символов, разрешены только латинские буквы",
+        });
+        setIsValid(false);
+      }
+    }
+    if (candidate === "password") {
+      if (passwValidate(password)) {
+        setErrors({
+          ...errors,
+          password: "",
+        });
+      } else {
+        setErrors({
+          ...errors,
+          password:
+            "Пароль должен быть от 6 до 12 символов латинские буквы и цифры",
+        });
+        setIsValid(false);
+      }
+    }
+    if (candidate === "secondPassword") {
+      if (secondPassword === password) {
+        setErrors({
+          ...errors,
+          secondPassword: "",
+        });
+      } else {
+        setErrors({
+          ...errors,
+          secondPassword: "Введённые пароли должны совпадать",
+        });
+        setIsValid(false);
+      }
+    }
 
-		tryValidSetState();
-	};
+    tryValidSetState();
+  };
 
-	const tryValidSetState = () => {
-		const { username, password, secondPassword } = userfield;
-		if (
-			!errors.password &&
-			!errors.username &&
-			!errors.secondPassword &&
-			username &&
-			password &&
-			secondPassword
-		) {
-			setIsValid(true);
-		}
-	};
+  const tryValidSetState = () => {
+    const { username, password, secondPassword } = userfield;
+    if (
+      !errors.password &&
+      !errors.username &&
+      !errors.secondPassword &&
+      username &&
+      password &&
+      secondPassword
+    ) {
+      setIsValid(true);
+    }
+  };
 
-	useEffect(() => {
-		const { username, password, secondPassword } = errors;
-		if (password) {
-			return setNotice(password), setOpenSnack(true);
-		}
-		if (username) {
-			return setNotice(username), setOpenSnack(true);
-		}
-		if (secondPassword) {
-			return setNotice(secondPassword), setOpenSnack(true);
-		}
-		setOpenSnack(false);
-	}, [errors]);
+  useEffect(() => {
+    const { username, password, secondPassword } = errors;
+    if (password) {
+      return setNotice(password), setOpenSnack(true);
+    }
+    if (username) {
+      return setNotice(username), setOpenSnack(true);
+    }
+    if (secondPassword) {
+      return setNotice(secondPassword), setOpenSnack(true);
+    }
+    setOpenSnack(false);
+  }, [errors]);
 
-	return (
-		<>
-			<form className='auth__form' onSubmit={registr}>
-				<p className='auth__form__paragraph'>Регистрация</p>
-				<div className='auth__form__bodyForm'>
-					<label className='auth__form_loginandpassword'>
-						Логин:
-						<input
-							className={
-								(errors?.username &&
-									"auth__form__textfield wrongtextfield") ||
-								"auth__form__textfield"
-							}
-							type='text'
-							placeholder='username'
-							name='username'
-							autoComplete='off'
-							value={userfield.username}
-							onChange={(e) =>
-								setUserField({
-									...userfield,
-									username: e.target.value,
-								})
-							}
-							onBlur={blurHandler}
-						/>
-					</label>
+  return (
+    <>
+      <form className='auth__form' onSubmit={registr}>
+        <p className='auth__form__paragraph'>Регистрация</p>
+        <div className='auth__form__bodyForm'>
+          <label className='auth__form_loginandpassword'>
+            Логин:
+            <input
+              className={
+                (errors?.username &&
+                  "auth__form__textfield wrongtextfield") ||
+                "auth__form__textfield"
+              }
+              type='text'
+              placeholder='username'
+              name='username'
+              autoComplete='off'
+              value={userfield.username}
+              onChange={(e) =>
+                setUserField({
+                  ...userfield,
+                  username: e.target.value,
+                })
+              }
+              onBlur={blurHandler}
+            />
+          </label>
 
-					<label className='auth__form_loginandpassword'>
-						<span>Пароль:</span>
-						<input
-							className={
-								(errors?.password &&
-									"auth__form__textfield wrongtextfield") ||
-								"auth__form__textfield"
-							}
-							type={showPass}
-							name='password'
-							placeholder='password'
-							value={userfield.password}
-							onChange={(e) => {
-								setUserField({
-									...userfield,
-									password: e.target.value,
-								});
-							}}
-							onBlur={blurHandler}
-						/>
-						<i
-							title={
-								showPass !== "text"
-									? "Показать пароль"
-									: "Скрыть пароль"
-							}
-							onClick={() => showPassfunction()}
-							className='auth__form_showPassword'>
-							{showPass !== "text" ? (
-								<RemoveRedEyeIcon />
-							) : (
-								<VisibilityOffIcon />
-							)}
-						</i>
-					</label>
+          <label className='auth__form_loginandpassword'>
+            <span>Пароль:</span>
+            <input
+              className={
+                (errors?.password &&
+                  "auth__form__textfield wrongtextfield") ||
+                "auth__form__textfield"
+              }
+              type={showPass}
+              name='password'
+              placeholder='password'
+              value={userfield.password}
+              onChange={(e) => {
+                setUserField({
+                  ...userfield,
+                  password: e.target.value,
+                });
+              }}
+              onBlur={blurHandler}
+            />
+            <i
+              title={
+                showPass !== "text"
+                  ? "Показать пароль"
+                  : "Скрыть пароль"
+              }
+              onClick={() => showPassfunction()}
+              className='auth__form_showPassword'>
+              {showPass !== "text" ? (
+                <RemoveRedEyeIcon />
+              ) : (
+                <VisibilityOffIcon />
+              )}
+            </i>
+          </label>
 
-					<label className='auth__form_loginandpassword flex flwrap'>
-						Повторите пароль:
-						<input
-							className={
-								(errors?.secondPassword &&
-									"auth__form__textfield wrongtextfield") ||
-								"auth__form__textfield"
-							}
-							type={showPass}
-							name='secondPassword'
-							placeholder='password'
-							value={userfield.secondPassword}
-							onChange={(e) => {
-								setUserField({
-									...userfield,
-									secondPassword: e.target.value,
-								});
-							}}
-							onBlur={blurHandler}
-						/>
-					</label>
-				</div>
-				<div className='auth__form__botomblok'>
-					<button disabled={!isValid} className='auth__form__submit'>
-						Зарегистрироваться
-					</button>
-					<Link to='/'>Авторизоваться</Link>
-				</div>
-				<Snack
-					open={openSnack}
-					setOpen={setOpenSnack}
-					severity='warning'
-					message={notice}
-				/>
-			</form>
-		</>
-	);
+          <label className='auth__form_loginandpassword flex flwrap'>
+            Повторите пароль:
+            <input
+              className={
+                (errors?.secondPassword &&
+                  "auth__form__textfield wrongtextfield") ||
+                "auth__form__textfield"
+              }
+              type={showPass}
+              name='secondPassword'
+              placeholder='password'
+              value={userfield.secondPassword}
+              onChange={(e) => {
+                setUserField({
+                  ...userfield,
+                  secondPassword: e.target.value,
+                });
+              }}
+              onBlur={blurHandler}
+            />
+          </label>
+        </div>
+        <div className='auth__form__botomblok'>
+          <button disabled={!isValid} className='auth__form__submit'>
+            Зарегистрироваться
+          </button>
+          <Link to='/'>Авторизоваться</Link>
+        </div>
+        <Snack
+          open={openSnack}
+          setOpen={setOpenSnack}
+          severity='warning'
+          message={notice}
+        />
+      </form>
+    </>
+  );
 };
 
 export default Signup;
